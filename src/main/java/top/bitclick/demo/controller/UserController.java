2c3e3dfa1bc75cf44c542abb53018cedc7fd26b0
xieyingchao <xieyingchao@hydee.cn>
2020-12-10 16:36:59 +0800

    change UserController

--- src/main/java/top/bitclick/demo/controller/UserController.java
+++ src/main/java/top/bitclick/demo/controller/UserController.java
@@ -1,9 +1,16 @@
 package top.bitclick.demo.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+import top.bitclick.demo.entity.User;
+import top.bitclick.demo.mapper.UserMapper;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +23,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+	@Autowired
+	private UserMapper userMapper;
+	
+
+	@GetMapping("/hello")
+	public String sayHello(){
+		return "hello world";
+	}
+
+	@GetMapping("/select/{name}")
+	public List<User> getUser(@PathVariable String name){
+		return userMapper.selectAllByName(name);
+	}
 
 }
 
