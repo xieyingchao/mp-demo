8dc6dc384d28b4ece8ecc2c198bb740416d10d8a
xieyingchao <xieyingchao@hydee.cn>
2020-12-14 17:24:29 +0800

    add two test file.

---
+++ src/test/java/top/bitclick/demo/service/UserServiceTest.java
@@ -0,0 +1,16 @@
+package top.bitclick.demo.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Test
+    public void test01(){
+        System.out.println("test");
+    }
+}
---
+++ src/test/java/top/bitclick/demo/service/UserServiceTestByXYC.java
@@ -0,0 +1,18 @@
+package top.bitclick.demo.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @description: TODO
+ * @author: Xie Yingchao
+ * @time: 2020/12/14 17:17
+ */
+@SpringBootTest
+public class UserServiceTestByXYC {
+
+    @Test
+    public void test01(){
+        System.out.println("test02");
+    }
+}
